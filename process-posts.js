6d6e4540f45758ab94b80c21dc71b0e72e0237a9
Mat Pustizzi <mathew.pustizzi@gmail.com>
2018-08-02 09:02:45 -0400

    basic image fetching wip

--- process-posts.js
+++ process-posts.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const createThrottle = require("async-throttle");
 const stringify = require("json-stringify-safe");
+const download = require("image-downloader");
 
 let throttle = createThrottle(5); // only process 5 posts at a time
 
@@ -49,14 +50,38 @@ let formatHtml = function(input, $) {
   return input.html();
 };
 
+let fetchImages = (image, i) => {
+  // todo - pass post slug to this fn
+  return throttle(async () => {
+    let options = {
+      url: image.attribs.src,
+      dest: "./output/images"
+    };
+    try {
+      const { filename, image } = await download.image(options);
+      console.log(filename); // => /path/to/dest/image.jpg
+    } catch (e) {
+      console.error(e);
+    }
+  });
+};
+
 let processPosts = (post, i) =>
   throttle(async () => {
     console.log(`Processing post # ${i + 1} : ${post.slug}`);
+
     let $ = await cheerio.load(post.content, { xmlMode: true });
+    let rootEl = $.root();
+    let images = rootEl.find("img").get();
+
+    Promise.all(images.map(fetchImages)).catch(err => console.log(err));
+
     let results = await {
       title: toTitleCase(post.title),
-      content: formatHtml($.root(), $)
+      thumbnail: `${post.featured_image}?resize=500,500`,
+      content: formatHtml(rootEl, $)
     };
+
     fs.writeFileSync(
       `./output/${post.slug}.json`,
       stringify({ results }, null, 2)
